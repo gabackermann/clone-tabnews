edbc5fa8762428f790524158fc195d55275916ad
Gabriel Ackermann <gab_ackermann@hotmail.com>
2025-01-19 08:30:06 -0300

    adicionando mais aulas de python do dia 18

---
+++ curso_python/aula12-13.py
@@ -0,0 +1,11 @@
+nome = 'Gabriel Ackermann'
+altura = 1.83
+peso = 152
+imc = peso / (altura * altura)
+
+linha_1 = f'{nome} tem {altura:.2f} de altura'
+linha_2 = f'pesa {peso} quilos e seu IMC é {imc:.2f}'
+
+
+print(linha_1)
+print(linha_2)
