57f62dc79009265eb5a586186882cb627aa4d70f
Gabriel Ackermann <gab_ackermann@hotmail.com>
2025-02-04 05:32:12 -0300

    merged files

---
+++ curso_python/curso_python/aula12.py
@@ -0,0 +1,10 @@
+nome = 'Gabriel Ackermann'
+altura = 1.83
+peso = 152
+imc = peso / (altura * altura)
+
+
+
+print(nome, 'tem', altura ,'de altura,')
+print('pesa', peso , "quilos e seu IMC é")
+print(imc)
---
+++ curso_python/curso_python/aula3.py
@@ -0,0 +1,19 @@
+'''
+DocString
+
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+'''
+
+# Aspas simples
+print('Gabriel Ackermann')
+#Aspas duplas
+print("Gabriel Ackermann")
+
+# Escape
+print("Gabriel  \"Ackermann\"")
+
+# r
+print(r"Gabriel  \"Ackermann\"")
